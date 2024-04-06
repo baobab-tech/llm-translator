3bc58b91a650e91575b02022268648e178ccd69a
Olivier <olivier@millsit.ca>
2024-04-06 10:49:26 -0600

    add cohere command-r-plus

--- lib/ai/cohere.ts
+++ lib/ai/cohere.ts
@@ -1,11 +1,19 @@
 import { ChatCohere } from "@langchain/cohere";
 import { genericLLMCall } from "./generic";
 
-export const cohere = async (data) => {
+const MODEL_MAP = {
+  "cmdr": "command-r", // $0.5/$1.5 /M tokens
+  "cmdrplus": "command-r-plus", // $3/$15 /M tokens
+}
+
+type Models = keyof typeof MODEL_MAP;
+
+
+export const cohere = (model: Models) => async (data) => {
   const chatModel = new ChatCohere({
     apiKey: process.env.COHERE_API_KEY,
     temperature: data.temperature,
-    model: "command-r",
+    model: MODEL_MAP[model],
   });
   return genericLLMCall(chatModel, data);
 };
